215337792c24ccad3618a4ed2ff3036877023aab
Laura Beutler <labeutler@gmail.com>
2023-02-05 10:09:04 -0500

    create and add code to schema.sql and auth.js.

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS a_techies_blog_db;
+
+CREATE DATABASE a_techies_blog_db;
---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+//Require Authorization for using site
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+module.exports = withAuth;
