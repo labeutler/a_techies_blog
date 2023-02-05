d19cabbcccd3245478ec7ebb7b04c1e67a535532
Laura Beutler <labeutler@gmail.com>
2023-02-05 11:40:54 -0500

    creating login, logout and signup in .js and starting handlebar requests.

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // collect information needed to log in
+    const usernameEl = document.querySelector('#username-login');
+    const passwordEl = document.querySelector('#password-login');
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                username: usernameEl.value, 
+                password: passwordEl.value, 
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Please try logging in again.');
+        }
+    };
+}
+
+document
+.querySelector('#login-form')
+.addEventListener('submit', loginFormHandler);
---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    // collect information needed to log in
+    const usernameEl = document.querySelector('#username-login');
+    const passwordEl = document.querySelector('#password-login');
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                username: usernameEl.value, 
+                password: passwordEl.value, 
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Please try signing up again.');
+        }
+    };
+}
+
+document
+.querySelector('#signup-form')
+.addEventListener('submit', signupFormHandler);
