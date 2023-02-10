b6b940de8ad9685970b25f651a4de4c60be32850
Laura Beutler <labeutler@gmail.com>
2023-02-10 00:05:40 -0500

    Revert " I GOT MY COMMENTS WORKING.  darn backslashgit add -A!  I will get even one day!"
    This reverts commit 06bae895a1935dba9afc411e53691f9a92f18898.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Comment } = require('../../models/');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // get all comments
@@ -12,18 +12,20 @@ const withAuth = require('../../utils/auth');
 //         });
 // });
 
+// create a new comment
+router.post('/', withAuth, (req, res) => {
 
-router.post('/', withAuth, async (req, res) => {
-    try {
-      const newComment = await Comment.create({
-        ...req.body,
-        userId: req.session.userId,
-      });
-      res.json(newComment);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+        const body = req.body; 
+        Comment.create({
+            ...body,
+            userId: req.session.userId,
+        })
+        // res.json(commentData);
+        .then((newComment) => res.json(newComment))
+        .catch((err) => {
+        res.status(400).json(err);
+    });
+});
 
 
 module.exports = router;
